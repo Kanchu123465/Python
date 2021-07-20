60ef63d258d82a82b13ebdb6c15b7e3229822fb3
Kanchu123465 <kanchanagoudar123@gmail.com>
2021-07-20 15:46:38 +0530

    lists

--- list.py
+++ list.py
@@ -1,2 +1,11 @@
 names=["kanchana","kavya","suresh","soumya",21,22,23,24]
-print(names[2:4])
+del names[3]
+//print(names[2:4])
+
+print(names)
+names.remove("akshay")
+print(names)
+names.pop(1)
+print(names)
+names.clear()
+print(names)
